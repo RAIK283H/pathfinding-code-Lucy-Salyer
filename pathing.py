0a2e27aec1fd74f50ce51ff402c8c885183f0803
Lucy-Salyer <143023791+Lucy-Salyer@users.noreply.github.com>
2024-10-06 17:58:01 -0500

    Finished!

--- pathing.py
+++ pathing.py
@@ -16,7 +16,33 @@ def get_test_path():
 
 
 def get_random_path():
-    return [1,2]
+    curr_graph = graph_data.graph_data[global_game_data.current_graph_index]
+    assert len(curr_graph) >= 2, "The graph is null or not big enough to generate a path"
+    target = global_game_data.target_node[global_game_data.current_graph_index]
+    end_node = len(curr_graph) - 1
+    curr_node = 0
+    path = []
+    while curr_node != target:
+        if(target in curr_graph[curr_node][1]):
+            next_node = target
+            path.append(next_node)
+        else:
+            next_node = random.choice(curr_graph[curr_node][1])
+            path.append(next_node) 
+
+        curr_node = next_node
+
+    while curr_node != end_node:
+        if(end_node in curr_graph[curr_node][1]):
+            next_node = end_node
+            path.append(next_node)
+        else:
+            next_node = random.choice(curr_graph[curr_node][1])
+            path.append(next_node) 
+        curr_node = next_node
+
+    assert len(path) >= 0, "No path was generated"
+    return path
 
 
 def get_dfs_path():
